8bc32aa663e741dfc58b24d3ced8cd6e82493eb9
Marc Mentis <mmentis@optonline.net>
2014-11-16 13:43:43 -0500

    First Commit

---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "users/new" do
+  before(:each) do
+    assign(:user, stub_model(User,
+      :firstname => "MyString",
+      :lastname => "MyString",
+      :email => "MyString",
+      :facility => "MyString",
+      :role => "MyString"
+    ).as_new_record)
+  end
+
+  it "renders new user form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", users_path, "post" do
+      assert_select "input#user_firstname[name=?]", "user[firstname]"
+      assert_select "input#user_lastname[name=?]", "user[lastname]"
+      assert_select "input#user_email[name=?]", "user[email]"
+      assert_select "input#user_facility[name=?]", "user[facility]"
+      assert_select "input#user_role[name=?]", "user[role]"
+    end
+  end
+end
---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "users/show" do
+  before(:each) do
+    @user = assign(:user, stub_model(User,
+      :firstname => "Firstname",
+      :lastname => "Lastname",
+      :email => "Email",
+      :facility => "Facility",
+      :role => "Role"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    rendered.should match(/Firstname/)
+    rendered.should match(/Lastname/)
+    rendered.should match(/Email/)
+    rendered.should match(/Facility/)
+    rendered.should match(/Role/)
+  end
+end
